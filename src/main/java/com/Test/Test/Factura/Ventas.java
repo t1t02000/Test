1f4f2c9098d2acc500d95c5423d69ced07fcb08d
Roberto <rugl2715@gmail.com>
2023-12-20 12:06:19 -0600

    Se agregaron Loggers de info al Directorio y a las Ventas

--- src/main/java/com/Test/Test/Factura/Ventas.java
+++ src/main/java/com/Test/Test/Factura/Ventas.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Service;
 import com.Test.Test.Persona.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 
@@ -20,17 +23,20 @@ public class Ventas
 
      public Factura FindFacturaByPersona(Integer id)
     {
+        log.info("Buscando factura de persona con ID: {}", id);
         List<Factura> facturas = findFacturasByPersona(id);
         return facturas.isEmpty() ? null : facturas.get(0);
     }
 
     public List<Factura> findFacturasByPersona(Integer personaId) 
     {
+        log.info("Buscando todas las facturas de la persona con ID: {}", personaId);
         return ventasRepo.findByPersonaId(personaId);
     }
 
     public void storeFactura(Factura factura)
     {
+        log.info("Creando nueva factura: {}", factura);
         validarPersonaExistente(factura);
         factura.validarCampos();
         ventasRepo.save(factura);
@@ -39,11 +45,12 @@ public class Ventas
     private void validarPersonaExistente(Factura factura) {
         if (factura.getPersona() == null || factura.getPersona().getId() == null ||
             !personaRepository.existsById(factura.getPersona().getId())) {
-            throw new IllegalArgumentException("El id de la persona en la factura no existe en la base de datos");
+                log.error("El id de la persona en la factura no existe en la base de datos");
+                throw new IllegalArgumentException("El id de la persona en la factura no existe en la base de datos");
         }
     }
+
+
+
     
-
-
-
 }
--- src/main/java/com/Test/Test/Persona/Directorio.java
+++ src/main/java/com/Test/Test/Persona/Directorio.java
@@ -1,35 +1,43 @@
 package com.Test.Test.Persona;
 
+
 import org.springframework.stereotype.Service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
+
 public class Directorio 
 {
 
     private final PersonaRepository personRepo;
 
+
     public Persona FindpersonByid(Integer id)
     {
+        log.info("Buscando persona con ID: {}", id);
         return personRepo.findById(id).orElse(null);
     }
 
     public List<Persona> FindPersona()
     {
+        log.info("Obteniendo todas las personas");
         return (List<Persona>) personRepo.findAll();
     }
 
     public void borrarPersona(Integer id) 
     {
+        log.info("Borrando persona con ID: {}", id);
         personRepo.deleteById(id);
     }
 
     public void storePersona(Persona persona)
     {
+        log.info("Se creo una nueva persona: {}", persona);
         persona.validarCamposObligatorios();
         personRepo.save(persona);
     }
